c4e47bd48e1da55fb998054dfd1e3ca1871ee0c6
James Parrott <80779630+JamesParrott@users.noreply.github.com>
2025-07-07 12:35:54 +0100

    Fix bug from now introduction of email.utils.parseaddr(...,strict=True)

--- src/maintainers_and_authors/core.py
+++ src/maintainers_and_authors/core.py
@@ -5,7 +5,6 @@ import sys
 from typing import Iterable, Iterator
 
 import requests
-from tqdm import tqdm
 
 
 def _version_tuple_from_str(s: str) -> tuple:
@@ -14,15 +13,14 @@ def _version_tuple_from_str(s: str) -> tuple:
 
 def _parse_mail_boxes(mail_boxes: str) -> Iterator[tuple[str, str]]:
     while mail_boxes:
-        name, email_ = email.utils.parseaddr(mail_boxes)
+        
+        name, email_ = email.utils.parseaddr(mail_boxes, strict=False)
 
         if (name, email_) == ('', ''):
             break
 
-        if email_.endswith('.noreply.github.com'):
-            continue
-
-        yield name, email_
+        if not email_.endswith('.noreply.github.com'):
+            yield name, email_
 
         mail_boxes = (mail_boxes.partition(f'<{email_}>')[2]
                                 .lstrip()
@@ -43,20 +41,24 @@ def _email_addresses(
 
     # print('Processing projects: ', end='')
 
-    tqdm_wrapped_project_names = tqdm(project_names)
-
-    for project_name in tqdm_wrapped_project_names:
+    for project_name in project_names:
 
         project_name = project_name.rstrip()
 
-        tqdm_wrapped_project_names.write(f'Processing: {project_name}')
+        if not project_name:
+            continue
 
-        # print(f'{project_name}, ', end='', flush=True)
-        response = requests.get(f'https://www.wheelodex.org/json/projects/{project_name}/data')
+        url = f'https://www.pypi.org/pypi/{project_name}/json'
+
+
+        print(f'{url=}, ', flush=True)
+        # response = requests.get(f'https://www.wheelodex.org/json/projects/{project_name}/data')
+        response = requests.get(url)
 
         response.raise_for_status()
 
-        meta_data = response.json()['data']['dist_info']['metadata']
+        # meta_data = response.json()['data']['dist_info']['metadata']
+        meta_data = response.json()['info']
 
 
         names, emails = [], []
@@ -66,8 +68,10 @@ def _email_addresses(
                     _parse_mail_boxes(meta_data.get('maintainer_email') or ''),
                     _parse_mail_boxes(meta_data.get('author_email') or ''),
                     ):
+            print(f'{name=}, {email_=}')
             names.append(name)
             emails.append(email_)
+
 
         if names and emails:
             project_data = dict(
